b225a7c3657a0385a2c62b6708ab6f00004c1861
Sasha <sasha.tsatryan.01@gmail.com>
2021-02-17 22:03:04 +0400

    Finally

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const hbs = require("hbs");
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-console.log(333);
 const app = express();
 const public = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
